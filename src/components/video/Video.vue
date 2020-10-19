8714094d36476da4a0b57a40897f792e43349e45
asa108 <55392369+asa108@users.noreply.github.com>
2020-10-19 14:53:18 +0900

    scroll 時のエラー解決 scrollする度にエラーが出ていたのでsettimeoutを使って解決。 hls動画だとbuffered  が消されるというエラーが出る。

--- src/components/video/Video.vue
+++ src/components/video/Video.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div ref="videoContainer" id="videoContainer">
     <video
       ref="video"
       id="video"
       controls
+      preload="none"
       webkit-playsinline
       muted
       autoplay
@@ -19,24 +20,37 @@ import Hls from "hls.js";
 export default {
   data() {
     return {
-      // hlsjsを機能させるための初期化処理を格納した変数
       hls: new Hls(),
       height: window.innerHeight,
     };
   },
   methods: {
     playVide() {
-      this.height = window.innerHeight;
+      // this.height = window.innerHeight;
+      this.height = window.height;
       const video = document.getElementById("video");
 
       if (Hls.isSupported()) {
         this.hls = new Hls();
         this.hls.loadSource(
-          "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
+          "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
         );
         this.hls.attachMedia(video);
-        video.play();
+        // video.play();
+        setTimeout(() => {
+          video.play();
+        }, 1000);
       }
+    },
+    pullCallback() {
+      const patternOverflow = this.$refs.videoContainer;
+      if (patternOverflow.scrollTop > 0) {
+        setTimeout(() => {
+          patternOverflow.scrollTop = 0;
+        }, 200);
+      }
+
+      this.$emit("pull-callback", null);
     },
   },
   mounted() {
