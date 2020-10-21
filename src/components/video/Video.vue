1548aa6483a24d2faabaa1839ac6525bf72d618f
asa108 <55392369+asa108@users.noreply.github.com>
2020-10-21 16:47:53 +0900

    hls動画を再生する mahoumake用のm3u8はCROSエラーで再生できない

--- src/components/video/Video.vue
+++ src/components/video/Video.vue
@@ -1,16 +1,7 @@
 <template>
   <div ref="videoContainer" id="videoContainer">
-    <video
-      ref="video"
-      id="video"
-      controls
-      preload="none"
-      webkit-playsinline
-      muted
-      autoplay
-    ></video>
-
-    <p>window height: {{ height }}</p>
+    <video ref="video" id="video" controls webkit-playsinline muted autoplay>
+    </video>
   </div>
 </template>
 
@@ -21,57 +12,27 @@ export default {
   data() {
     return {
       hls: new Hls(),
-      height: window.innerHeight,
     };
   },
   methods: {
     playVide() {
-      // this.height = window.innerHeight;
-      this.height = window.height;
-      const video = document.getElementById("video");
-
-      if (Hls.isSupported()) {
-        this.hls = new Hls();
-        this.hls.loadSource(
-          "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-        );
-        this.hls.attachMedia(video);
-        // video.play();
-        setTimeout(() => {
-          video.play();
-        }, 1000);
+        const video = document.getElementById("video");
+        if (Hls.isSupported()) {
+          this.hls = new Hls();
+          this.hls.loadSource(
+            "https://storage.googleapis.com/mahou_make/Assets/MAHOUMAKE_HLS/MAHOUMAKE_EYESHAOW.m3u8"
+          );
+          this.hls.attachMedia(video);
+          setTimeout(() => {
+            video.play();
+          }, 1000);
+        
       }
-    },
-    pullCallback() {
-      const patternOverflow = this.$refs.videoContainer;
-      if (patternOverflow.scrollTop > 0) {
-        setTimeout(() => {
-          patternOverflow.scrollTop = 0;
-        }, 200);
-      }
-
-      this.$emit("pull-callback", null);
     },
   },
   mounted() {
     window.addEventListener("scroll", this.playVide);
-    // window.addEventListener("scroll", this.playVideo);
   },
-  // mounted() {
-  //   const video = document.getElementById("video");
-  //   if (Hls.isSupported()) {
-  //     this.hls = new Hls();
-  //     // this.hls.loadSource("./video/playlist.m3u8");
-  //     // this.hls.loadSource(
-  //     //   "https://storage.googleapis.com/mahou_make/Assets/MAHOUMAKE_HLS/MAHOUMAKE_EYESHAOW.m3u8"
-  //     // );
-  //     this.hls.loadSource(
-  //       "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
-  //     );
-  //     this.hls.attachMedia(video);
-  //     video.play();
-  //   }
-  // },
 };
 </script>
 
