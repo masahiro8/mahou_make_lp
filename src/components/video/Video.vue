80cbd460c97fef32e1fa04398a9d29b27f4789cd
asa108 <55392369+asa108@users.noreply.github.com>
2020-10-26 11:39:54 +0900

    safari でautoplayできない safari でautoplayできないが、ボタンを押すと再生する。その他のデバイスでは自動再生できる

--- src/components/video/Video.vue
+++ src/components/video/Video.vue
@@ -22,6 +22,7 @@ export default {
   data() {
     return {
       hls: new Hls(),
+      clear_id: null,
     };
   },
   methods: {
@@ -33,20 +34,82 @@ export default {
           "https://storage.googleapis.com/mahou_make/Assets/MAHOUMAKE_HLS/MAHOUMAKE_EYESHAOW.m3u8"
         );
         this.hls.attachMedia(video);
-        setTimeout(() => {
-          video.play();
-        }, 1000);
+
+        // Show loading animation.
+        var playPromise = video.play();
+
+        if (playPromise !== undefined) {
+          playPromise
+            .then((_) => {
+              // Automatic playback started!
+              // Show playing UI.
+              this.clear_id = null;
+              if (this.clear_id) clearTimeout(this.clear_id);
+              this.clear_id = setTimeout(() => {
+                video.play();
+              }, 1000);
+              console.log("video play");
+            })
+            .catch((error) => {
+              // Auto-play was prevented
+              // Show paused UI.
+              console.log("error");
+            });
+        }
       } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
         video.src =
           "https://storage.googleapis.com/mahou_make/Assets/MAHOUMAKE_HLS/MAHOUMAKE_EYESHAOW.m3u8";
         video.addEventListener("canplay", function () {
-          video.play();
+          // Show loading animation.
+          var playPromise = video.play();
+
+          if (playPromise !== undefined) {
+            playPromise
+              .then((_) => {
+                // Automatic playback started!
+                // Show playing UI.
+                this.clear_id = null;
+                if (this.clear_id) clearTimeout(this.clear_id);
+                this.clear_id = setTimeout(() => {
+                  video.play();
+                }, 1000);
+                console.log("video play");
+              })
+              .catch((error) => {
+                // Auto-play was prevented
+                // Show paused UI.
+                console.log("error");
+              });
+          }
         });
       }
     },
   },
   mounted() {
     window.addEventListener("scroll", this.playVide);
+    // const video = document.getElementById("video");
+
+    // // Show loading animation.
+    // var playPromise = video.play();
+
+    // if (playPromise !== undefined) {
+    //   playPromise
+    //     .then((_) => {
+    //       // Automatic playback started!
+    //       // Show playing UI.
+    //       this.clear_id = null;
+    //       if (this.clear_id) clearTimeout(this.clear_id);
+    //       this.clear_id = setTimeout(() => {
+    //         video.play();
+    //       }, 1000);
+    //       console.log("video play");
+    //     })
+    //     .catch((error) => {
+    //       // Auto-play was prevented
+    //       // Show paused UI.
+    //       console.log("error");
+    //     });
+    // }
   },
 };
 </script>
